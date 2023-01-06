7039079dfc296093ac9dd5c149d6bf18d332b96a
tamasiscoming <nvntamas@gmail.com>
2023-01-06 01:28:10 +0100

    add project skeleton

---
+++ Deloitte_Interview/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Reflection;
+using System.Security.Cryptography;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        ChooseTask();
+    }
+
+    public static void ChooseTask()
+    {
+        Console.WriteLine("Choose Task\n" +
+            "Press 1 for SQL Task\n" +
+            "Press 2 for Programming Task");
+
+        int taskNo = int.Parse(Console.ReadKey().KeyChar.ToString());
+
+        switch (taskNo)
+        {
+            case 1:
+                Console.Clear();
+                break;
+            case 2:
+                Console.Clear();
+                break;
+            default:
+                Console.Clear();
+                Console.WriteLine("Choose a valid option...");
+                ChooseTask();
+                break;
+        }
+    }
+}
