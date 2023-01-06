9a7b2369e3a88c7b02d48844dcb8950c9584a2a2
tamasiscoming <nvntamas@gmail.com>
2023-01-06 01:29:37 +0100

    add console app logic

---
+++ Deloitte_Interview/Logic/ConsoleAppLogic.cs
@@ -0,0 +1,54 @@
+﻿using Deloitte_Interview.Logic;
+
+public class ConsoleAppLogic
+{
+    public static void Run()
+    {
+        Console.WriteLine("Deliotte Console App Interview Task");
+
+        List<string> input = InputHandler.GetInputFromUser();
+
+        ProcessInputs(input);
+
+        Console.WriteLine(string.Join("\n", input));
+
+    }
+
+    static void ProcessInputs(List<string> inputs)
+    {
+        for (int i = 0; i < inputs.Count; i++)
+        {
+            int number;
+            if (int.TryParse(inputs[i], out number))
+            {
+                if (IsPrime(number))
+                {
+                    inputs[i] = number + " - THIS IS A PRIME NUMBER";
+                }
+            }
+            else
+            {
+                int length = inputs[i].Length;
+                inputs[i] = inputs[i] + " - " + "Making an impact that matters –Deloitte".Substring(0, length);
+            }
+        }
+    }
+
+    static bool IsPrime(int number)
+    {
+        if (number < 2)
+        {
+            return false;
+        }
+
+        for (int i = 2; i <= Math.Sqrt(number); i++)
+        {
+            if (number % i == 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
---
+++ Deloitte_Interview/Logic/InputHandler.cs
@@ -0,0 +1,55 @@
+﻿namespace Deloitte_Interview.Logic
+{
+    internal static class InputHandler
+    {
+        internal static List<string> GetInputFromUser()
+        {
+            List<string> inputs = new List<string>();
+
+            for (int i = 0; i < 2; i++)
+            {
+                Console.WriteLine($"================{i + 1}.Input================");
+                Console.WriteLine("Enter a word or number (w/n):");
+                string inputType = Console.ReadLine();
+
+                while (inputType != "w" && inputType != "n")
+                {
+                    Console.WriteLine("Invalid input. Please enter either 'w' for word or 'n' for number:");
+                    inputType = Console.ReadLine();
+                }
+
+                if (inputType == "w")
+                {
+                    Console.WriteLine("Enter a word (10-45 characters):");
+                    string word = Console.ReadLine();
+
+                    while (word.Length < 10 || word.Length > 45)
+                    {
+                        Console.WriteLine("Invalid input. Please enter a word with 10-45 characters:");
+                        word = Console.ReadLine();
+                    }
+
+                    inputs.Add(word);
+                }
+                else if (inputType == "n")
+                {
+                    Console.WriteLine("Enter a number (10-9999):");
+                    string numberString = Console.ReadLine();
+                    int number;
+
+                    while (!int.TryParse(numberString, out number) || number < 10 || number > 9999)
+                    {
+                        Console.WriteLine("Invalid input. Please enter a number between 10 and 9999:");
+                        numberString = Console.ReadLine();
+                    }
+
+                    inputs.Add(numberString);
+                }
+            }
+
+            Console.WriteLine("Inputs: " + string.Join(", ", inputs));
+
+            return inputs;
+        }
+    }
+}
